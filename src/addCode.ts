c8af10cf57ba042ff05cbc782a905cea04a83111
Jasleen8801 <jkaur1_be21@thapar.edu>
2023-11-09 19:30:08 +0530

    hob gya

--- src/addCode.ts
+++ src/addCode.ts
@@ -3,14 +3,6 @@ import axios from 'axios';
 
 export function activate(context: vscode.ExtensionContext) {
   const API_URI: string = "http://localhost:3000";
-  
-  function formatCode(code: string): string {
-    const indentedCode = code.replace(/(?:\r\n|\r|\n)/g, '\\n');
-    const tabSpaces = vscode.workspace.getConfiguration('editor').get('tabSize');
-    // console.log(tabSpaces);
-    const formattedCode = indentedCode.replace(`/\s{${tabSpaces}}/g`, '\\t'); 
-    return formattedCode;
-  }
 
   let addCodeDisposable = vscode.commands.registerCommand('snapfolio.addCode', async () => {
     const bot_id = vscode.workspace.getConfiguration('snapfolio').get('botId');
@@ -30,39 +22,47 @@ export function activate(context: vscode.ExtensionContext) {
           prompt: "Enter a description for the code block",
           placeHolder: "Enter a description",
         }) || '';
-        const formattedCode = formatCode(selectedText);
-        console.log(formattedCode);
         const ans: string = await vscode.window.showInputBox({
           prompt: "Do you want to add the output to the document?",
           placeHolder: "Enter (y/n)",
         }) || 'n';
         // console.log(ans);
-        if(ans === 'y'){
+        if(ans === 'y' || ans === 'Y'){
           flag = true;
           const config = vscode.workspace.getConfiguration();
           config.update('terminal.integrated.copyOnSelection', true, vscode.ConfigurationTarget.Global);
           const selectedOutput = await vscode.env.clipboard.readText();
-          const formattedOutput = formatCode(selectedOutput) || '';
-          console.log(formattedOutput);
 
           const output = await vscode.window.showInputBox({
             prompt: "Check the output selected in the terminal",
-            value: formattedOutput,
+            value: selectedOutput,
           });
           console.log(output);
 
           const response = await axios.post(`${API_URI}/notion/appendCode`, {
             bot_id: bot_id,
             flag: flag,
-            code: formattedCode,
+            code: selectedText,
             subheading: subheading,
             description: description,
             theme: theme,
             fontSize: fontSize,
             backgroundColor: backgroundColor,
-            output: formattedOutput
+            output: selectedOutput
           });
           console.log(response.data);
+        } else if(ans === 'n'){
+          const response = await axios.post(`${API_URI}/notion/appendcode`, {
+            bot_id: bot_id,
+            flag: flag,
+            code: selectedText,
+            subheading: subheading,
+            description: description,
+            theme: theme,
+            fontSize: fontSize,
+            backgroundColor: backgroundColor
+          });
+          console.log(response);
         }
       }
     }
